3d04eeddccd6881630cc92aa7133c58ad08bc254
Bakhramovaa <bahramovamuhayyo3@gmail.com>
2023-10-24 15:15:08 +0500

    3_oy-4_dars

---
+++ 3_oy-5_dars/js/app.js
@@ -0,0 +1,43 @@
+// let a = "1234";
+// console.log(parseInt(a) + 3);
+// console.log(parseFloat(a));
+// console.log(+a);
+// console.log(-a);
+// console.log(a * 3);
+// console.log(a / 2);
+// console.log(a - 0);
+// console.log(3 + a);
+
+// number, boolean =>string
+// let a = 1234.45;
+
+// String() constructor function
+// console.log(String(a));
+// console.log(a + "");
+// console.log(typeof (a + ""));
+
+// // Number => String
+// console.log(a.toString());
+// console.log(a.toFixed(5));
+
+// Operators
+// Arithmetic
+// +, -, *, /, %, ++, --, **, // => comment
+
+// console.log(10 % 3); (qoldigini natijasi)
+// let b = 1;
+// console.log(b);
+// console.log(++b);
+// console.log(b);
+// console.log(b);
+// console.log(--b);
+// console.log(b);
+// console.log(2 ** 8);
+
+// Comparison =>taqqoslash
+// >, <, <=, >=, ==, ===, !=, !==
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 < 1);
+// console.log(1 >= 1);
+// console.log(1 == 2);
---
+++ 3_oy-5_dars/js/uyga-vazifa.js
@@ -0,0 +1,13 @@
+// let age = prompt(
+//   "Yoshingiz nechida ekanligin ayting men sizni nechinchi yil ekanligingizni va 3 yildan keyin nechi yoshga to'lishingizni aytaman?"
+// );
+// alert(2023 - age);
+// alert(Number(age) + 3);
+// let age1 = prompt("nechi yoshga kirishni istaysiz");
+// let answer2 = alert(age1 - age + "yil" + " " + "qoldi");
+
+let age = prompt("Yoshingiz nechida");
+alert(2023 - age);
+alert(Number(age) + 3);
+let istak = prompt("Nechi yil umr ko'rishni hohlaysiz?");
+let hisob = alert(istak - age + " " + "yil qoldi In Shaa Alloh");
